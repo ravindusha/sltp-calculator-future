3d2982cf48058aa42283ea0a6750ad12779bde6f
Ravindu Shanaka <ravzshanaka@gmail.com>
2021-11-21 23:15:27 +0530

    calculate SL/TP by price

--- src/App.js
+++ src/App.js
@@ -13,8 +13,15 @@ import "./styles.css";
 
 export default function App() {
   const [entry, setEntry] = useState(0);
-  const [slP, setSlP] = useState(0);
-  const [tpP, setTpP] = useState(0);
+  const [slPercentage, setSlPercentage] = useState(0);
+  const [tpPercentage, setTpPercentage] = useState(0);
+
+  const [slPriceForLong, setSlPriceForLong] = useState(0);
+  const [tpPriceForLong, setTpPriceForLong] = useState(0);
+
+  const [slPriceForShort, setSlPriceForShort] = useState(0);
+  const [tpPriceForShort, setTpPriceForShort] = useState(0);
+
   const [tpPriceLong, setTpPriceLong] = useState(0);
   const [slPriceLong, setSlPriceLong] = useState(0);
   const [tpPriceShort, setTpPriceShort] = useState(0);
@@ -28,19 +35,62 @@ export default function App() {
 
   useEffect(() => {
     if (entry) {
-      const slLong = (Number(entry) * (100 - Number(slP))) / 100;
+      setSlPriceForShort(0);
+      setTpPriceForShort(0);
+      setSlPriceForLong(0);
+      setTpPriceForLong(0);
+
+      const slLong = (Number(entry) * (100 - Number(slPercentage))) / 100;
       setSlPriceLong(slLong.toFixed(4));
-      const tpLong = (Number(entry) * (100 + Number(tpP))) / 100;
+      const tpLong = (Number(entry) * (100 + Number(tpPercentage))) / 100;
       setTpPriceLong(tpLong.toFixed(4));
-      const slShort = (Number(entry) * (100 + Number(slP))) / 100;
+      const slShort = (Number(entry) * (100 + Number(slPercentage))) / 100;
       setSlPriceShort(slShort.toFixed(4));
-      const tpShort = (Number(entry) * (100 - Number(tpP))) / 100;
+      const tpShort = (Number(entry) * (100 - Number(tpPercentage))) / 100;
       setTpPriceShort(tpShort.toFixed(4));
 
-      setProfit((Number(amount) * Number(tpP) * Number(leverage)) / 100);
-      setLoss((Number(amount) * Number(slP) * Number(leverage)) / 100);
+      setProfit(
+        (Number(amount) * Number(tpPercentage) * Number(leverage)) / 100
+      );
+      setLoss((Number(amount) * Number(slPercentage) * Number(leverage)) / 100);
     }
-  }, [slP, tpP, entry, amount, leverage]);
+  }, [slPercentage, tpPercentage, entry, amount, leverage]);
+
+  useEffect(() => {
+    if (!entry) return;
+
+    setSlPriceForShort(0);
+    setTpPriceForShort(0);
+
+    const SLPercent = (
+      100 -
+      (Number(slPriceForLong) * 100) / Number(entry)
+    ).toFixed(2);
+    const TPPercent = (
+      (Number(tpPriceForLong) * 100) / Number(entry) -
+      100
+    ).toFixed(2);
+    setSlPercentage(SLPercent);
+    setTpPercentage(TPPercent);
+  }, [slPriceForLong, tpPriceForLong]);
+
+  useEffect(() => {
+    if (!entry) return;
+
+    setSlPriceForLong(0);
+    setTpPriceForLong(0);
+
+    const SLPercent = (
+      (Number(slPriceForShort) * 100) / Number(entry) -
+      100
+    ).toFixed(2);
+    const TPPercent = (
+      100 -
+      (Number(tpPriceForShort) * 100) / Number(entry)
+    ).toFixed(2);
+    setSlPercentage(SLPercent);
+    setTpPercentage(TPPercent);
+  }, [slPriceForShort, tpPriceForShort]);
 
   const copyToClipboard = (text) => {
     try {
@@ -94,8 +144,8 @@ export default function App() {
               <InputGroup>
                 <Input
                   type="text"
-                  onChange={(e) => setSlP(e.target.value)}
-                  value={slP}
+                  onChange={(e) => setSlPercentage(e.target.value)}
+                  value={slPercentage}
                   className="eqw"
                 />
                 <InputRightAddon children="%" />
@@ -108,11 +158,84 @@ export default function App() {
               <InputGroup>
                 <Input
                   type="text"
-                  onChange={(e) => setTpP(e.target.value)}
-                  value={tpP}
+                  onChange={(e) => setTpPercentage(e.target.value)}
+                  value={tpPercentage}
                   className="eqw"
                 />
                 <InputRightAddon children="%" />
+              </InputGroup>
+            </Flex>
+          </div>
+        </Box>
+        <Box borderWidth="1px" padding="15px">
+          <Flex>
+            <Text fontSize="lg" color="green" fontWeight="bold">
+              Long
+            </Text>
+          </Flex>
+          <div className="row res">
+            <Flex direction="row" alignItems="center">
+              <Text fontSize="lg" className="eqw">
+                SL
+              </Text>
+              <InputGroup>
+                <Input
+                  type="text"
+                  onChange={(e) => setSlPriceForLong(e.target.value)}
+                  value={slPriceForLong}
+                  className="eqw"
+                />
+                <InputRightAddon children="Price" />
+              </InputGroup>
+            </Flex>
+            <Flex direction="row" alignItems="center">
+              <Text fontSize="lg" className="eqw">
+                TP
+              </Text>
+              <InputGroup>
+                <Input
+                  type="text"
+                  onChange={(e) => setTpPriceForLong(e.target.value)}
+                  value={tpPriceForLong}
+                  className="eqw"
+                />
+                <InputRightAddon children="Price" />
+              </InputGroup>
+            </Flex>
+          </div>
+          <Divider padding="2px" />
+          <Flex>
+            <Text fontSize="lg" color="red" fontWeight="bold">
+              Short
+            </Text>
+          </Flex>
+          <div className="row res">
+            <Flex direction="row" alignItems="center">
+              <Text fontSize="lg" className="eqw">
+                SL
+              </Text>
+              <InputGroup>
+                <Input
+                  type="text"
+                  onChange={(e) => setSlPriceForShort(e.target.value)}
+                  value={slPriceForShort}
+                  className="eqw"
+                />
+                <InputRightAddon children="Price" />
+              </InputGroup>
+            </Flex>
+            <Flex direction="row" alignItems="center">
+              <Text fontSize="lg" className="eqw">
+                TP
+              </Text>
+              <InputGroup>
+                <Input
+                  type="text"
+                  onChange={(e) => setTpPriceForShort(e.target.value)}
+                  value={tpPriceForShort}
+                  className="eqw"
+                />
+                <InputRightAddon children="Price" />
               </InputGroup>
             </Flex>
           </div>
@@ -200,7 +323,7 @@ export default function App() {
                 Profit
               </Text>
               <Text fontSize="md" color="green.600" fontWeight="bold">
-                {profit}
+                {profit.toFixed(2)}
               </Text>
             </div>
 
@@ -209,7 +332,7 @@ export default function App() {
                 Loss
               </Text>
               <Text fontSize="md" color="red.600" fontWeight="bold">
-                {loss}
+                {loss.toFixed(2)}
               </Text>
             </div>
           </div>
